be32d9d80d9a5211e2b75acd99d0573df1bea830
Retzudo <christoph.willinger@gmail.com>
2017-10-19 11:58:04 +0200

    index

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.get('/:code', (req, res) => {
     let { code } = req.params;
-    if (!code) {
-        res.render('index');
-    } else {
-        res.render('display');
-    }
+    res.render('display');
+});
+
+const PORT = process.env.SOAPSTONE_PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
