7ed103b0cd14130cbc82d73d1358eea2d583921c
Retzudo <christoph.willinger@gmail.com>
2017-10-19 11:45:12 +0200

    webpack; frontend

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './frontend-js/build.js',
+  output: {
+    path: __dirname + '/public/js',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
